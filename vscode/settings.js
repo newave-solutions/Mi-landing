d96d331dc00ecd02812c4e446199f107de9059d4
Eladio Limones <limones.rdz.eladio@gmail.com>
2025-04-06 09:19:35 -0600

    Create settings.js

---
+++ vscode/settings.js
@@ -0,0 +1,9 @@
+{
+    "python.defaultInterpreterPath": "${workspaceFolder}/venv/Scripts/python.exe",
+    "python.analysis.extraPaths": ["${workspaceFolder}"],
+    "python.languageServer": "Pylance",
+    "python.analysis.typeCheckingMode": "basic",
+    "python.analysis.diagnosticMode": "workspace",
+    "python.linting.enabled": true,
+    "python.linting.pylintEnabled": true
+}
